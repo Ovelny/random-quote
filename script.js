2c31f1fa9244f0b12616350ca63b7a1ff37f4c1c
ovelny <ovelny@ovelny.lan>
2016-08-11 23:48:29 +0200

    api request w/ jsonp

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
 
-	var newQuote = new XMLHttpRequest();
-	newQuote.open('GET', 'http://api.forismatic.com/api/1.0/getQuote&key=457653&format=jsonp&lang=en', true);
-
-	newQuote.onload = function() {
-		var responseText = newQuote.responseText;
-		console.log(responseText);
-	};
-
-
-	newQuote.onerror = function() {
-		console.log('Error');
-	};
-
-	newQuote.send();
-
-});
+ $.ajax ({
+   url :"http://quotes.stormconsultancy.co.uk/quotes/random.json?callback=foo",
+   type: "GET",
+   dataType : "jsonp",
+   success : function(getQuote) {
+     console.log(getQuote);
+   },
+ });
+ });
