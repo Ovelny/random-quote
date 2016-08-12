bf00789ae7bd5939f616a46aa1ab5012fbdc9199
ovelny <ovelny@ovelny.lan>
2016-08-12 23:54:07 +0200

    semantic ui

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
 
- $.ajax ({
-   url :"http://quotes.stormconsultancy.co.uk/quotes/random.json?callback=foo",
-   type: "GET",
-   dataType : "jsonp",
-   success : function(getQuote) {
-     console.log(getQuote);
-   },
- });
- });
+	$("#generateQuote, #shareTwitter").click(function() {
+		$(this).transition('jiggle');
+	});
+
+	$.ajax({
+		url: "http://quotes.stormconsultancy.co.uk/quotes/random.json?callback=foo",
+		type: "GET",
+		dataType: "jsonp",
+		success: function(getQuote) {
+			console.log(getQuote);
+		},
+	});
+});
