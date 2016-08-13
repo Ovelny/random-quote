eef049ccad753f21d22a2e2baa9954a69cc8ee71
ovelny <ovelny@ovelny.lan>
2016-08-13 23:13:30 +0200

    json stringify

--- script.js
+++ script.js
@@ -4,12 +4,25 @@ $(document).ready(function() {
 		$(this).transition('jiggle');
 	});
 
+	/*$("#generateQuote").click(function() {
+		$(".presentation").remove();
+		$(".quote").html(getQuote);
+		$("p").removeClass("hidden");
+	});*/
+
 	$.ajax({
 		url: "http://quotes.stormconsultancy.co.uk/quotes/random.json?callback=foo",
 		type: "GET",
 		dataType: "jsonp",
 		success: function(getQuote) {
-			console.log(getQuote);
+
+			$("#generateQuote").click(function() {
+
+				$(".presentation").remove();
+				$(".quote-content").html(JSON.stringify(getQuote));
+				$("p").removeClass("hidden");
+
+			});
 		},
 	});
 });
