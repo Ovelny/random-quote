40693e78f2861bd73675f67de87b38958a7116b2
Thibault Vercoutere <thibault.vercoutere@gmail.com>
2016-08-20 17:13:13 +0200

    animations disabled & tweet share

--- script.js
+++ script.js
@@ -6,27 +6,46 @@ $(document).ready(function() {
 
 	$("#generateQuote").click(function() {
 
+
+
 		$.ajax({
 			url: "http://quotes.stormconsultancy.co.uk/quotes/random.json?callback=foo",
 			type: "GET",
 			dataType: "jsonp",
 			success: function(getQuote) {
 
+				
+
 				$(".presentation").remove();
 
-				$(".quote-content").animate({'opacity': 0}, 350);
-				$(".quote-author").animate({'opacity': 0}, 350);
-				
-				$(".quote-content").html(JSON.stringify(getQuote.quote)).animate({'opacity': 100}, 350);
-				$(".quote-author").html(JSON.stringify(getQuote.author).replace(/\"/g,"")).animate({'opacity': 100}, 350);
+				$(".twitter-share-button").prop("href", "https://twitter.com/intent/tweet?text=" + twtShrQuote + " ~ " + twtShrAuthor);
 
-				
+				/*$(".quote-content").animate({
+					'opacity': 0
+				}, 350);*/
+
+				/*$(".quote-author").animate({
+					'opacity': 0
+				}, 350);*/
+
+				$(".quote-content").html(JSON.stringify(getQuote.quote)).animate({
+					'opacity': 100
+				}, 350);
+
+				$(".quote-author").html(JSON.stringify(getQuote.author).replace(/\"/g, "")).animate({
+					'opacity': 100
+				}, 350);
+
+				var twtShrQuote = $("p.quote-content").text();
+				var twtShrAuthor = $("p.quote-author").text();
+
+
 				$("p").removeClass("hidden");
-
+				console.log(twtShrQuote);
 
 			},
 		});
 	});
 
-	
+
 });
