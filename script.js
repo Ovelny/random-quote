4c02b03658b65df68b06ecf3b2699aad0f9df536
Thibault Vercoutere <this.is.ovelny@gmail.com>
2016-08-20 18:43:55 +0200

    twitter character limit

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ $(document).ready(function() {
 	});
 
 	$("#generateQuote").click(function() {
-		
+
 		$.ajax({
 			url: "http://quotes.stormconsultancy.co.uk/quotes/random.json?callback=foo",
 			type: "GET",
 			dataType: "jsonp",
 			success: function(getQuote) {
-
-				$(".presentation").remove();
 
 				/*$(".quote-content").animate({
 					'opacity': 0
@@ -22,20 +20,27 @@ $(document).ready(function() {
 					'opacity': 0
 				}, 350);*/
 
-				$(".quote-content").html(JSON.stringify(getQuote.quote)).animate({
-					'opacity': 100
-				}, 350);
+				if (getQuote.quote.length + getQuote.author.length <= 140) {
 
-				$(".quote-author").html(JSON.stringify(getQuote.author).replace(/\"/g, "")).animate({
-					'opacity': 100
-				}, 350);
+					$(".presentation").remove();
 
-				var twtShrQuote = $("p.quote-content").text();
-				var twtShrAuthor = $("p.quote-author").text();
+					$(".quote-content").html(JSON.stringify(getQuote.quote)).animate({
+						'opacity': 100
+					}, 350);
 
-				$(".twitter-share-button").prop("href", "https://twitter.com/intent/tweet?text=" + twtShrQuote + " ~ " + twtShrAuthor);
+					$(".quote-author").html(JSON.stringify(getQuote.author).replace(/\"/g, "")).animate({
+						'opacity': 100
+					}, 350);
 
-				$("p").removeClass("hidden");
+					var twtShrQuote = $("p.quote-content").text();
+					var twtShrAuthor = $("p.quote-author").text();
+
+					$(".twitter-share-button").prop("href", "https://twitter.com/intent/tweet?text=" + twtShrQuote + " ~ " + twtShrAuthor);
+
+					$("p").removeClass("hidden");
+
+				}
+				console.log(getQuote.quote.length + getQuote.author.length);
 
 			},
 		});
